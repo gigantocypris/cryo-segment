27397f18adc41e7b18b06d48268a761c70aca9db
Vidya Ganapati <vganapati@gmail.com>
2024-01-30 15:20:53 -0800

    visualization with annotations

--- download_data.py
+++ download_data.py
@@ -7,47 +7,79 @@ import numpy as np
 import imageio
 
 
-def download_dataset(dest_path, dataset_id, dataset_name):
+def download_dataset(dest_path, dataset_id, dataset_name, download_annotations=True):
     print('Downloading the dataset...')
     # Instantiate a client, using the data portal GraphQL API by default
     client = Client()
 
     # Find all tomograms and annotations with a dataset id and dataset name
     tomos = Tomogram.find(client, [Tomogram.tomogram_voxel_spacing.run.dataset.id == dataset_id, Tomogram.tomogram_voxel_spacing.run.name == dataset_name])
-    annotations = Annotation.find(client, [Annotation.tomogram_voxel_spacing.run.dataset.id == dataset_id, Annotation.tomogram_voxel_spacing.run.name == dataset_name])
-
-    for tomo,anno in zip(tomos, annotations):
-        print(tomo.name)
-        
+    
+    for tomo in tomos:
+        print('Tomogram name: ' + tomo.name)
         # Download a tomogram in the MRC format
         # The dest path needs to be a file name, not a directory
         tomo.download_mrcfile(dest_path=dest_path)
-        # Download annotations
-        breakpoint()
-        anno.download(dest_path=dest_path+'_annotations')
 
-def visualize_tomogram(dest_path, dataset_id, dataset_name, gif_spacing):
+    if download_annotations:
+        annotations = Annotation.find(client, [Annotation.tomogram_voxel_spacing.run.dataset.id == dataset_id, Annotation.tomogram_voxel_spacing.run.name == dataset_name])
+
+        for ind, anno in enumerate(annotations):
+            print(anno)
+            anno.download(dest_path=dest_path+'_annotations_' + str(ind))
+
+def visualize_tomogram(dest_path, dataset_id, dataset_name, gif_spacing, annotation=False, dest_path_suffix=''):
+
+    dest_path = dest_path + dest_path_suffix
+
     with mrcfile.open(dest_path) as mrc:
         print(mrc.data.shape)
         z, x, y = mrc.data.shape
-        vmin = np.percentile(mrc.data, 5)
-        vmax = np.percentile(mrc.data, 95)
+        if annotation:
+            vmin = mrc.data.min()
+            vmax = mrc.data.max()
+        else:
+            vmin = np.percentile(mrc.data, 5)
+            vmax = np.percentile(mrc.data, 95)
         plt.figure()
         plt.imshow(mrc.data[z//2, :, :], vmin = vmin, vmax = vmax, cmap = 'gray')
-        plt.savefig('../output-cryo-segment/dataset_' + str(dataset_id) + '_' + str(dataset_name) + '.png')
+        plt.savefig('../output-cryo-segment/dataset_' + str(dataset_id) + '_' + str(dataset_name) + dest_path_suffix + '.png')
 
         # Create gif for the tomogram
         
-
         images = []
         for i in range(0,z,gif_spacing):
             image_i = np.copy(mrc.data[i, :, :])
-            image_i[image_i < vmin] = vmin
-            image_i[image_i > vmax] = vmax
+            if annotation:
+                pass
+            else:
+                image_i[image_i < vmin] = vmin
+                image_i[image_i > vmax] = vmax
             image_i = (image_i - vmin) / (vmax - vmin)
             images.append(image_i*255)
-        imageio.mimsave('../output-cryo-segment/dataset_' + str(dataset_id) + '_' + str(dataset_name) + '.gif', images)
+        imageio.mimsave('../output-cryo-segment/dataset_' + str(dataset_id) + '_' + str(dataset_name) + dest_path_suffix + '.gif', images)
+    return images
 
+def create_annotated_gif(images, annotations, dataset_id, dataset_name, dest_path_suffix, save_single_images=False):
+    annotated_images = []
+    for ind, (image, annotation) in enumerate(zip(images, annotations)):
+        # annotation = annotation*3//255
+        annotation = np.flipud(annotation*3//255) # np.flipud needed for TE13
+        breakpoint()
+        red_image = np.copy(image)
+        green_image = np.copy(image)
+        blue_image = np.copy(image)
+        for color in range(1,4):
+            red_image[annotation==color] = 255*(color==1)
+            green_image[annotation==color] = 255*(color==2)
+            blue_image[annotation==color] = 255*(color==3)
+        annotated_image = np.stack([red_image, green_image, blue_image], axis=2)
+        annotated_image = annotated_image.astype(np.uint8)
+        annotated_images.append(annotated_image)
+        if ind == len(images)//2:
+            imageio.imsave('../output-cryo-segment/dataset_' + str(dataset_id) + '_' + str(dataset_name) + dest_path_suffix + '.png', annotated_image)
+    annotated_images = np.stack(annotated_images,axis=0)
+    imageio.mimsave('../output-cryo-segment/dataset_' + str(dataset_id) + '_' + str(dataset_name) + dest_path_suffix + '.gif', annotated_images)
 
 if __name__ == "__main__":
     # Inputs
--- plot_annotations.py
+++ plot_annotations.py
@@ -3,12 +3,12 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Polygon
-from download_data import download_dataset, visualize_tomogram
+from download_data import download_dataset, visualize_tomogram, create_annotated_gif
 # best dataset on cryo ET portal for membrane segmentation ground truth: https://cryoetdataportal.czscience.com/datasets/10010
 
 # Inputs
 dataset_id = 10010
-dataset_name = 'TE2'
+dataset_name = 'TE13'
 gif_spacing = 5
 
 # Output path for the tomogram file (not a directory)
@@ -18,37 +18,8 @@ dest_path = '../output-cryo-segment/dataset_' + str(dataset_id) + '_' + str(data
 if os.path.isfile(dest_path):
     print('The dataset has already been downloaded.')
 else:
-    print('Downloading the dataset...')
     download_dataset(dest_path, dataset_id, dataset_name)
 
-visualize_tomogram(dest_path, dataset_id, dataset_name, gif_spacing)
-
-breakpoint()
-# Load the image
-image = plt.imread('path_to_your_image.jpg')
-
-# Parse the segmentation annotations from the file
-with open('path_to_your_annotations.json', 'r') as f:
-    annotations = json.load(f)
-
-# Create a figure and axis object
-fig, ax = plt.subplots()
-
-# Plot the image
-ax.imshow(image)
-
-# Iterate through each annotation
-for annotation in annotations:
-    # Extract location and rotation matrix
-    location = np.array([annotation['location']['x'], annotation['location']['y']])
-    rotation_matrix = np.array(annotation['xyz_rotation_matrix'])
-    
-    # Example code to draw a polygon for each annotation
-    # You need to implement the logic based on your annotation format
-    # Here, we assume the annotation contains points of a polygon
-    polygon = np.array([[0, 0], [100, 0], [100, 100], [0, 100]])  # Example polygon, replace with your annotation data
-    transformed_polygon = np.dot(polygon, rotation_matrix.T) + location
-    ax.add_patch(Polygon(transformed_polygon, alpha=0.5, closed=True, fill=True, edgecolor='r'))
-
-# Show the image with segmentation annotations
-plt.show()
+images = visualize_tomogram(dest_path, dataset_id, dataset_name, gif_spacing)
+annotations = visualize_tomogram(dest_path, dataset_id, dataset_name, gif_spacing, annotation=True, dest_path_suffix = '_annotations_0')
+create_annotated_gif(images, annotations, dataset_id, dataset_name, dest_path_suffix = '_annotations_0_image')
